a8f2a399569da9bddcd1b9c3b72b34329990fd2d
Morgan Frotten <mmfrotten@gmail.com>
2022-10-14 14:51:45 -0400

    names appearing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
+  const [charData, setCharData] = useState([]);
+
+  useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+  .then(res => {
+    setCharData(res.data);
+    console.log(charData[0].name);
+  })
+  .catch(err => {
+    console.err(err);
+  })
+  .finally(() => {
+    console.log("yeehaw");
+  })
+}, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,6 +29,12 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        charData && charData.map(item => {
+          let charName = item.name;
+          return <Character charName={charName}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+function Character(props) {
+
+
+    return (
+        <div>
+            <p>Name: {props.charName}</p>
+        </div>
+    )
+}
+
+export default Character
